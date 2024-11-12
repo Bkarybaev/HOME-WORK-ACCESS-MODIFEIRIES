dfcc3fa0d80ceae39052d1ce53134ffb88e0fdea
Bkarybaev <baielkarybaev@gmail.com>
2024-11-12 13:46:30 +0600

    first commit

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Circle {
+    static final double PI = 3.4;
+    private double radius;
+
+    static double area(double radius) {
+        return PI * (radius * radius);
+    }
+
+    static double circumference(double radius) {
+        return PI * 2 * radius;
+    }
 }
